79fbf6cd0008fa2a5e15aee4c3f0d9a3d0a4ef7d
Chris Harrington <chrisharrington99@gmail.com>
2018-08-20 16:16:01 -0600

    Initial commit. Proof of concept.

---
+++ app.ts
@@ -0,0 +1,38 @@
+import 'module-alias/register';
+import * as fs from 'fs';
+import * as express from 'express';
+
+const app = express();
+
+app.get('/video', function(req, res) {
+    const path = '\\\\bravo\\Media\\Kid\'s Movies\\Ballerina (2016)\\Ballerina (2016).mkv'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1] 
+            ? parseInt(parts[1], 10)
+            : fileSize-1
+        const chunksize = (end-start)+1
+        const file = fs.createReadStream(path, {start, end})
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(path).pipe(res)
+    }
+});
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
